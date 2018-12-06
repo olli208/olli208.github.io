c84f3d3a73b20c6dce78e342cf10bde7dad8eb45
Oliver <oliveroenang@gmail.com>
2018-12-06 15:31:07 +0100

    portfolio overhaul

--- js/main.js
+++ js/main.js
@@ -1,50 +1,21 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+(function () {
+	fizz()
+	whatYearIsIt()
 
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+	function fizz() { 
+		for (var i = 1; i < 101; i++) {
+			// my method
+			if (i % (3*5) == 0) console.log("FizzBuzz");
+			else if (i % 3 == 0) console.log("Fizz");
+			else if (i % 5 == 0) console.log("Buzz");
+			else console.log(i);
 
+			// fastest method
+			// console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i)
+		}
+	}
 
-$(document).ready(function() {
-    console.log('THIS PORTFOLIO IS OUTDATED, FOR MY LATEST WORK CHECK MY GITHUB: https://github.com/olli208')
-    //containerexp animatie
-    $('.containerexp').mouseenter(function() {
-        $(this).animate({"margin-top":"-0.4em"}, 100);
-        //$(this).find('.cont').animate({"margin-top":"-0.1em"}, 100);
-        $(this).find('.glyphicon').animate({"margin-top":"-0.02em"}, 100);
-        $(this).css('background' , '#fff');
-        $(this).find('h3').css('color' , '#eb5070');
-        $(this).find('p').css('color' , '#656565');
-        $(this).find('p').css('display' , 'block');
-        $(this).find('.glyphicon').css('color' , '#fff');
-        
-        
-        //$(this).mouseleave(function() {
-        //    $(this).animate({"margin-top":"2.5em"}, 100)});
-    });
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-
+	function whatYearIsIt() {
+		document.querySelector('span.what-year-is-it').innerHTML = new Date().getFullYear()
+	}
+})();
